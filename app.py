f50ceacaeca241001255f8b2479bcd57517aa72d
Alexis <muroa1@uci.edu>
2021-02-07 23:40:44 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/Categories")
+def Categories():
+    return render_template("categories.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
