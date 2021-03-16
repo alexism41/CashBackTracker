34319ebee29cdff3d154668dfe0a1cb864bd977b
Alexis <muroa1@uci.edu>
2021-03-16 01:17:15 -0700

    changes second page from categories to "cards"

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, redirect, url_for, render_template
-from parse import getBestInEach
+from parse import getBestInEach, cardList
 
 app = Flask(__name__)
 deals = getBestInEach()
+data = cardList
+headers = ("Category", "Cash Back %")
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", deals = deals)
 
-@app.route("/Categories")
+@app.route("/Cards")
 def Categories():
-    return render_template("categories.html")
+    return render_template("categories.html", headers= headers, cList = data)
 
 if __name__ == "__main__":
     app.run(debug = True)
